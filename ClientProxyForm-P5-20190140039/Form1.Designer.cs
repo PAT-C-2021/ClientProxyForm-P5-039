96069646db506b156023b84c3624884e0be333b6
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2021-11-11 19:28:37 +0700

    Add project files.

---
+++ ClientProxyForm-P5-20190140039/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace ClientProxyForm_P5_20190140039
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHasil = new System.Windows.Forms.Button();
+            this.lBagi = new System.Windows.Forms.Label();
+            this.lKali = new System.Windows.Forms.Label();
+            this.lKurang = new System.Windows.Forms.Label();
+            this.lTambah = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHasil
+            // 
+            this.btnHasil.Location = new System.Drawing.Point(127, 170);
+            this.btnHasil.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHasil.Name = "btnHasil";
+            this.btnHasil.Size = new System.Drawing.Size(100, 21);
+            this.btnHasil.TabIndex = 25;
+            this.btnHasil.Text = "Hasil";
+            this.btnHasil.UseVisualStyleBackColor = true;
+            this.btnHasil.Click += new System.EventHandler(this.btnHasil_Click);
+            // 
+            // lBagi
+            // 
+            this.lBagi.AutoSize = true;
+            this.lBagi.Location = new System.Drawing.Point(410, 178);
+            this.lBagi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lBagi.Name = "lBagi";
+            this.lBagi.Size = new System.Drawing.Size(0, 13);
+            this.lBagi.TabIndex = 24;
+            // 
+            // lKali
+            // 
+            this.lKali.AutoSize = true;
+            this.lKali.Location = new System.Drawing.Point(410, 144);
+            this.lKali.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lKali.Name = "lKali";
+            this.lKali.Size = new System.Drawing.Size(0, 13);
+            this.lKali.TabIndex = 23;
+            // 
+            // lKurang
+            // 
+            this.lKurang.AutoSize = true;
+            this.lKurang.Location = new System.Drawing.Point(410, 113);
+            this.lKurang.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lKurang.Name = "lKurang";
+            this.lKurang.Size = new System.Drawing.Size(0, 13);
+            this.lKurang.TabIndex = 22;
+            // 
+            // lTambah
+            // 
+            this.lTambah.AutoSize = true;
+            this.lTambah.Location = new System.Drawing.Point(410, 83);
+            this.lTambah.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lTambah.Name = "lTambah";
+            this.lTambah.Size = new System.Drawing.Size(0, 13);
+            this.lTambah.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 127);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(102, 20);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 76);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(102, 20);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(275, 178);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Hasil Pembagian";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 144);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Hasil Perkalian";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Hasil Pengurangan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 83);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Hasil Penjumlahan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 127);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Angka 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Angka 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 310);
+            this.Controls.Add(this.btnHasil);
+            this.Controls.Add(this.lBagi);
+            this.Controls.Add(this.lKali);
+            this.Controls.Add(this.lKurang);
+            this.Controls.Add(this.lTambah);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kalkulator Matematika Sederhana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHasil;
+        private System.Windows.Forms.Label lBagi;
+        private System.Windows.Forms.Label lKali;
+        private System.Windows.Forms.Label lKurang;
+        private System.Windows.Forms.Label lTambah;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
